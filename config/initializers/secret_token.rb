e4a4720c8709977c386e768541733ebd089c0dc3
francordie <francordie@gmail.com>
2014-03-09 21:04:56 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Procrast::Application.config.secret_key_base = '9641504a0c808c3eb491711921853cdf5c229c3bf9e86a52d5cdea0550cb4238c79cee2a69292715151e143c36c82ba5e44d41f1957ae607196497f1da559022'
